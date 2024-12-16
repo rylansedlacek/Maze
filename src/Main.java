e50c7834c27e62cef66fb4e769dff3800bb79d44
Rylan Sedlacek <rsedlace@mail.umw.edu>
2024-12-16 12:52:58 -0500

    getting there

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 class Main {
     public static void main (String args[]) {
 
+        Scanner stdin = new Scanner(System.in);
+
+        System.out.println("DEFINE MAZE DIMENSIONS:");
+        System.out.println();
+        System.out.print("Rows: ");
+        int rows = stdin.nextInt();
+        System.out.print("Cols: ");
+        int cols = stdin.nextInt();
+
+        Maze maze = new Maze(rows, cols);
+        MazeGenerator generator = new MazeGenerator(maze);
+        generator.generate();
+
+        maze.printMaze();
+
+        // add solving next
 
     }
 }
--- src/Maze.java
+++ src/Maze.java
@@ -1,5 +1,48 @@
 class Maze {
 
+    int rows;
+    int cols;
+    Cell[][] grid;
+
+    public Maze(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Cell[rows][cols];
+        initGrid();
+    }
+
+    void initGrid() {
+        for (int i=0; i<rows; ++i) {
+            for (int j=0; j<cols; ++j) {
+                grid[i][j] = new Cell(i, j);
+            }
+        }
+    }
+
+    
+    public Cell getCell(int row, int col) {
+        if (row < 0 || col < 0 ||
+                row >= rows || col >= cols) {
+            return null;
+        }
+        return grid[row][col];
+    }
+
+    public Cell[][] getGrid() {
+        return this.grid;
+    }
+
+    public void printMaze() {
+
+    }
+
+    class Cell {
+
+
+
+
+
+    }
 
 
 
